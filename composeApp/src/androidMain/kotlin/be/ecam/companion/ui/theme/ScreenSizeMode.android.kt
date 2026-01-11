87472a9dedf64e35aa86982cfc64423013b01e94
crepinini <nirina.crepin@mainetti.com>
2026-01-11 11:04:58 +0100

    Android - Taking off forced screen and policy size

--- composeApp/src/androidMain/kotlin/be/ecam/companion/ui/theme/ScreenSizeMode.android.kt
+++ composeApp/src/androidMain/kotlin/be/ecam/companion/ui/theme/ScreenSizeMode.android.kt
@@ -1,10 +1,3 @@
 package be.ecam.companion.ui.theme
 
-import android.content.res.Resources
-
-internal actual fun platformScreenWidthDp(): Int? {
-    val metrics = Resources.getSystem().displayMetrics ?: return null
-    val density = metrics.density
-    if (density == 0f) return null
-    return (metrics.widthPixels / density).toInt()
-}
+internal actual fun platformScreenWidthDp(): Int? = null
